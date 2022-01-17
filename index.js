996f2dcd56b6351e44bb3cbbb413296b8017a741
Marianny Fidelis <mariannyfidelis@gmail.com>
2022-01-17 10:55:56 -0300

    Query Params e Route Params

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
 const express = require('express');
-
-//console.log(express)
-
 const server = express();
 
+// Query Params ==> São parâmetros que passamos na rota do seguinte modo: ?nome=Fulano
+// Exemplos Query Params - http://localhost:3000/curso?nome=Joana&idade=30
 server.get('/curso', (request, response) => {
-    console.log("Acessou");
-    
-    //Retorno de mensagem simples
-    //return response.send("Hello Marianny");
+    const nome = request.query.nome;
+    const idade = request.query.idade;
 
     //Retorno de um Json
-    return response.json({"user": 'Maria', "idade": 20});
+    return response.json({"nome": `${nome}`, "idade": `${idade}`});
+});
+
+const cursos = ['Python', 'NodeJS', 'Flutter', 'Insomnia']
+//Route Params ==> São parâmetros que passamos na rota do seguinte modo: /curso/2
+server.get("/curso/:id", (request, response) => {
+    const id = request.params.id;
+
+    return response.json({"id": `${id}`});
+});
+
+server.get('/cursos/:id', (request, response) => {
+
+    const indexEnviado = request.params.id;
+
+    return response.json({"id": request.params.id, "cursoEscolhido": cursos[indexEnviado]});
+    //return response.json(cursos[indexEnviado]);
 });
 
 server.listen(3000);
 
 
-//Query Params ==> São parâmetros que passamos na rota do seguinte modo: ?nome=Fulano
-
-//Route Params ==> São parâmetros que passamos na rota do seguinte modo: /curso/2
 
 //Request Body ==> Objeto que passamos com informações para a requisição
