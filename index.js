86cf9770f4e9a33a709b7a258fd761666488adb4
Marianny Fidelis <mariannyfidelis@gmail.com>
2022-01-17 15:02:27 -0300

    Criando um middleware

--- index.js
+++ index.js
@@ -14,27 +14,55 @@ server.get('/curso', (request, response) => {
     return response.json({"nome": `${nome}`, "idade": `${idade}`});
 });
 
+//Middleware Global
+//Criando um middleware que servirá como camada de tratamento intermediário dos dados
+server.use((request, response, next) => {
+    //Como não há hora o middleware tratará qualquer rota !
+    console.log(`Requisição chamada na URL: ${request.url}`);
+
+    return next();
+});
+
+//Vamos criar uma função de middleware para tratamento nas requisições;
+function checkCurso(request, response, next) {
+    if(!request.body.nomeDoCurso){
+        return response.status(404).json({"mensagem_erro": "Requisição errada e sem nome."});
+    }
+    return next();
+}
+
+function checkIndexCurso(request, response, next) {
+    const curso = cursos[request.params.index];
+
+    if(!curso){
+        return response.status(404).json({"mensagem_erro": "Requisição errada e sem índice válido"});
+    }
+
+    request.curso = cursos
+    return next();
+}
+
 server.get("/cursos", (request, response)=>{
 
     return response.json(cursos);
 });
 //Route Params ==> São parâmetros que passamos na rota do seguinte modo: /curso/2
-server.get("/curso/:id", (request, response) => {
-    const id = request.params.id;
+server.get("/curso/:index", (request, response) => {
+    const index = request.params.index;
 
-    return response.json({"id": `${id}`});
+    return response.json({"id": `${index}`});
 });
 
-server.get('/cursos/:id', (request, response) => {
+server.get('/cursos/:index', checkIndexCurso, (request, response) => {
 
-    const indexEnviado = request.params.id;
+    const indexEnviado = request.params.index;
 
-    return response.json({"id": request.params.id, "cursoEscolhido": cursos[indexEnviado]});
+    return response.json({"id": request.params.index, "cursoEscolhido": cursos[indexEnviado]});
     //return response.json(cursos[indexEnviado]);
 });
 
 //Vamos criar requisições do tipo POST - Inserir dados
-server.post("/cursos", (request, response) => {
+server.post("/cursos", checkCurso,(request, response) => {
     //Utilizando o Request body, ou seja enviando um JSON com os dados no body
     const { nomeDoCurso } = request.body;
     
@@ -44,7 +72,7 @@ server.post("/cursos", (request, response) => {
 });
 
 //Vamos criar requisições do tipo POST PUT - Atualizar dados
-server.put("/cursos/:index", (request, response) => {
+server.put("/cursos/:index", checkCurso, checkIndexCurso, (request, response) => {
     const indexEnviado = request.params.index;
     const { nomeDoCurso } = request.body;
     cursos[indexEnviado] = nomeDoCurso;
@@ -63,8 +91,7 @@ server.delete('/cursos/:index', (request, response) => {
     return response.json({"msg": "Curso deletado com sucesso ! ", "cursos": cursos});
 });
 
-
-
-server.listen(3000);
+//O servidor ouvirá na porta 3333
+server.listen(3333);
 
 //Request Body ==> Objeto que passamos com informações para a requisição
