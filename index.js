911df5abd18d316fcce9baf1dd38472abff80447
Marianny Fidelis <mariannyfidelis@gmail.com>
2022-01-17 12:20:40 -0300

    Criando um CRUD SIMPLES de Cursos

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const server = express();
+server.use(express.json());
+
+const cursos = ['Python', 'NodeJS', 'Flutter', 'Insomnia']
 
 // Query Params ==> São parâmetros que passamos na rota do seguinte modo: ?nome=Fulano
 // Exemplos Query Params - http://localhost:3000/curso?nome=Joana&idade=30
@@ -11,7 +14,10 @@ server.get('/curso', (request, response) => {
     return response.json({"nome": `${nome}`, "idade": `${idade}`});
 });
 
-const cursos = ['Python', 'NodeJS', 'Flutter', 'Insomnia']
+server.get("/cursos", (request, response)=>{
+
+    return response.json(cursos);
+});
 //Route Params ==> São parâmetros que passamos na rota do seguinte modo: /curso/2
 server.get("/curso/:id", (request, response) => {
     const id = request.params.id;
@@ -26,6 +32,38 @@ server.get('/cursos/:id', (request, response) => {
     return response.json({"id": request.params.id, "cursoEscolhido": cursos[indexEnviado]});
     //return response.json(cursos[indexEnviado]);
 });
+
+//Vamos criar requisições do tipo POST - Inserir dados
+server.post("/cursos", (request, response) => {
+    //Utilizando o Request body, ou seja enviando um JSON com os dados no body
+    const { nomeDoCurso } = request.body;
+    
+    //Adicionando na lista
+    cursos.push(nomeDoCurso);
+    response.json(cursos);
+});
+
+//Vamos criar requisições do tipo POST PUT - Atualizar dados
+server.put("/cursos/:index", (request, response) => {
+    const indexEnviado = request.params.index;
+    const { nomeDoCurso } = request.body;
+    cursos[indexEnviado] = nomeDoCurso;
+    
+    return response.json(cursos);
+
+});
+
+//Vamos criar requisições do tipo DELETE - Deletar dados
+server.delete('/cursos/:index', (request, response) => {
+
+    const { index } = request.params;
+
+    cursos.splice(index, 1);
+
+    return response.json({"msg": "Curso deletado com sucesso ! ", "cursos": cursos});
+});
+
+
 
 server.listen(3000);
 
